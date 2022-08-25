6003ead99f1b34f5463d42fc369879695cadcd5e
Iain Brown <codeiain@outlook.com>
2022-08-25 09:22:34 +0100

    renamed tests

--- text_adventure_ai/ner/ner_basic_direction_tests.py
+++ text_adventure_ai/ner/ner_basic_direction_tests.py
@@ -29,49 +29,49 @@ else:
 
 
 class NerBasicDirectionTest(unittest.TestCase):
-    def test_item_abacus(self):
+    def test_direction_north(self):
         test_text = "north"
         doc = ai.nlp(test_text)
         self.assertEqual(doc.ents[0].label_, "DIRECTION")
         self.assertEqual(str(doc.ents[0]), "north")  # add assertion
 
-    def test_item_abacus(self):
+    def test_direction_south(self):
         test_text = "south"
         doc = ai.nlp(test_text)
         self.assertEqual(doc.ents[0].label_, "DIRECTION")
         self.assertEqual(str(doc.ents[0]), "south")  # add assertion
 
-    def test_item_abacus(self):
+    def test_direction_east(self):
         test_text = "east"
         doc = ai.nlp(test_text)
         self.assertEqual(doc.ents[0].label_, "DIRECTION")
         self.assertEqual(str(doc.ents[0]), "east")  # add assertion
 
-    def test_item_abacus(self):
+    def test_direction_west(self):
         test_text = "west"
         doc = ai.nlp(test_text)
         self.assertEqual(doc.ents[0].label_, "DIRECTION")
         self.assertEqual(str(doc.ents[0]), "west")  # add assertion
 
-    def test_item_abacus(self):
+    def test_direction_up(self):
         test_text = "up"
         doc = ai.nlp(test_text)
         self.assertEqual(doc.ents[0].label_, "DIRECTION")
         self.assertEqual(str(doc.ents[0]), "up")  # add assertion
 
-    def test_item_abacus(self):
+    def test_direction_down(self):
         test_text = "down"
         doc = ai.nlp(test_text)
         self.assertEqual(doc.ents[0].label_, "DIRECTION")
         self.assertEqual(str(doc.ents[0]), "down")  # add assertion
 
-    def test_item_abacus(self):
+    def test_direction_left(self):
         test_text = "left"
         doc = ai.nlp(test_text)
         self.assertEqual(doc.ents[0].label_, "DIRECTION")
         self.assertEqual(str(doc.ents[0]), "left")  # add assertion
 
-    def test_item_abacus(self):
+    def test_direction_right(self):
         test_text = "right"
         doc = ai.nlp(test_text)
         self.assertEqual(doc.ents[0].label_, "DIRECTION")
